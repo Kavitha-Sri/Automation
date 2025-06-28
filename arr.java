c4b5d64f61286106f7500ec1cf355e313e5971e4
Kavitha Sri <Kavitha@ad.dynamicbiz.us>
2025-06-28 23:08:38 +0530

    victoryyyyy

---
+++ arr.java
@@ -0,0 +1,14 @@
+public class arr {
+    public static void main(String[] args) {
+        int[] a ={1,2,3,4,5};
+        int sum =0;
+        for(int b=0; b<5; b++)
+        {
+        sum = sum+a[b];
+        System.out.println(sum);
+
+        }
+        
+    }
+    
+}
